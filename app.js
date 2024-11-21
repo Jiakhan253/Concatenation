6f91cd58b5b474b4746566231683645c6fd53bea
Jiakhan253 <jiak66355@gmail.com>
2024-11-21 01:17:58 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+alert('Thanks!');
+
+var name = prompt ('Enter first name here');
+var name1 = prompt ('Enter last name here');
+
+console.log( name + name1);
+
+
+let num1 = 7;
+let num2 = "10";
+let total = ( num1+num2);
+
+console.log(total)
